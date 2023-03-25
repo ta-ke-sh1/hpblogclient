f90d10e536fa1e8e28b47f261db906930a2890f5
phongLH18715 <trunght.yrc@gmail.com>
2023-03-26 00:37:24 +0700

    responsive navigation bar

--- src/layouts/navbar.js
+++ src/layouts/navbar.js
@@ -1,14 +1,51 @@
 import React, { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import { Link } from "react-router-dom";
+import zIndex from "@mui/material/styles/zIndex";
 
 export default function MenuOverlay() {
-    const aboutMe = useRef(null);
+
+    const burgerNav = useRef(null);
+    const desktopNav = useRef(null);
+    const aboutNav = useRef(null);
+
     const [isOpenMenu, updateOpenMenu] = useState(false);
     const [isAnimating, updateAnimation] = useState(false);
 
+    window.addEventListener('resize', () => {
+        var t = gsap.timeline();
+        handleResize();
+    })
+
+    const handleResize = () => {
+        if (window.innerWidth < 700) {
+            gsap.to(burgerNav.current, {
+                duration: 0,
+                opacity: 1,
+                zIndex: 100000
+            })
+            gsap.to(desktopNav.current, {
+                duration: 0,
+                opacity: 0,
+                zIndex: 10
+            })
+        } else {
+            gsap.to(burgerNav.current, {
+                duration: 0,
+                opacity: 0,
+                zIndex: 10
+            })
+            gsap.to(desktopNav.current, {
+                duration: 0,
+                opacity: 1,
+                zIndex: 100
+            })
+        }
+    }
+
     useEffect(() => {
-        gsap.to(aboutMe.current, {
+        handleResize();
+        gsap.to(aboutNav.current, {
             duration: 0,
             zIndex: 1,
             xPercent: 100,
@@ -29,7 +66,7 @@ export default function MenuOverlay() {
             updateOpenMenu(false);
         }
 
-        gsap.to(aboutMe.current, {
+        gsap.to(aboutNav.current, {
             duration: 0.6,
             xPercent: isOpenMenu ? 100 : 0,
             ease: "easeOut",
@@ -65,33 +102,58 @@ export default function MenuOverlay() {
     return (
         <>
             <div className="custom-nav">
-                <div className="nav-item">Phuong Ha</div>
+                <div className="nav-item" style={{ zIndex: 100000 }}>Phuong Ha</div>
 
-                <div className="menu-box"></div>
-                <div className="menu-icon" onClick={openMenu}>
+                <div className="menu-icon" onClick={openMenu} ref={burgerNav}>
                     <span className="line" id={"first-line"}></span>
                     <span className="line" id={"second-line"}></span>
                     <span className="line" id={"third-line"}></span>
                 </div>
 
-                <div className="navigation-items" ref={aboutMe}>
-                    <div
-                        className="content-wrapper nav-item"
-                        id="first-nav-item">
-                        <div className="paragraph">my story</div>
-                    </div>
-                    <Link to={"/writings"}>
+                <div className="navigation-items-window" ref={desktopNav}>
+                    <Link style={{ textDecoration: 'none' }} to={"/writings"}>
+                        <div
+                            className="content-wrapper nav-item"
+                            id="first-nav-item">
+                            <div className="paragraph">my story</div>
+                        </div>
+                    </Link>
+                    <Link style={{ textDecoration: 'none' }} to={"/writings"}>
                         <div className="content-wrapper" id="first-second-item">
                             <div className="paragraph nav-item">
                                 my writings
                             </div>
                         </div>
                     </Link>
-                    <Link to={"/memories"}>
+                    <Link style={{ textDecoration: 'none' }} to={"/memories"}>
                         <div className="content-wrapper" id="first-third-item">
                             <div className="paragraph nav-item">my memories</div>
                         </div>
                     </Link>
+                </div>
+
+                <div className="navigation-items" ref={aboutNav}>
+                    <div className="bottom-align">
+                        <Link style={{ textDecoration: 'none' }} to={"/writings"}>
+                            <div
+                                className="content-wrapper nav-item"
+                                id="first-nav-item">
+                                <div className="paragraph">my story</div>
+                            </div>
+                        </Link>
+                        <Link style={{ textDecoration: 'none' }} to={"/writings"}>
+                            <div className="content-wrapper" id="first-second-item">
+                                <div className="paragraph nav-item">
+                                    my writings
+                                </div>
+                            </div>
+                        </Link>
+                        <Link style={{ textDecoration: 'none' }} to={"/memories"}>
+                            <div className="content-wrapper" id="first-third-item">
+                                <div className="paragraph nav-item">my memories</div>
+                            </div>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>
