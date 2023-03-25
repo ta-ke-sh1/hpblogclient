cadc8ce7f64c889ed6c5af70d8b7308af7968dd2
takesh1 <85833065+ta-ke-sh1@users.noreply.github.com>
2023-03-25 14:53:11 +0700

    added api call for image retrieval

--- src/layouts/memories.js
+++ src/layouts/memories.js
@@ -1,44 +1,51 @@
-import React, { useState, useEffect } from "react"
-import LightGallery from 'lightgallery/react';
+import React, { useState, useEffect } from "react";
+import LightGallery from "lightgallery/react";
+import useFetch from "../hooks/useFetch";
+import { host_url } from "../utils/utils";
 
 export default function Memories() {
-    const [items, getItems] = useState([]);
-
-    useEffect(() => {
-
-    }, [items]);
+    const { data, error, isLoaded } = useFetch(host_url + "/image");
 
     const getImages = () => {
-        return items.map((item) => {
+        return data.map((item) => {
             return (
                 <div
                     key={item.id}
                     data-lg-size={item.size}
                     className="gallery-item"
-                    data-src={item.src}
-                >
+                    data-src={host_url + "/images/" + item.name}>
                     <img
-                        style={{ maxWidth: '280px' }}
+                        style={{ maxWidth: "280px" }}
                         className="img-responsive"
-                        alt={item.src}
-                        src={item.thumb}
+                        alt={host_url + "/images/" + item.name}
+                        src={host_url + "/images/" + item.name}
                     />
                 </div>
             );
-        })
-    }
+        });
+    };
 
     return (
         <div className="custom-container mt-70">
             <div className="custom-wrapper h-30 t-center">
                 <div className="center-div w-40">
                     <div className="custom-row">
-                        <h1 className="display-font s-48 primary-color ml-5">my memories </h1>
-                        <span className="display-font s-24 primary-color ml-5">({items.length})</span>
+                        <h1 className="display-font s-48 primary-color ml-5">
+                            my memories{" "}
+                        </h1>
+                        <span className="display-font s-24 primary-color ml-5">
+                            ({data.length})
+                        </span>
                     </div>
                     <br />
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pellentesque massa risus, vitae dictum sem condimentum eu. Maecenas eu arcu ut elit feugiat lobortis at id libero. Vestibulum tempus eros ac diam ullamcorper elementum. Vestibulum ut euismod tortor. Donec dui velit, tempus ac magna at, aliquam laoreet sapien.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nullam pellentesque massa risus, vitae dictum sem
+                        condimentum eu. Maecenas eu arcu ut elit feugiat
+                        lobortis at id libero. Vestibulum tempus eros ac diam
+                        ullamcorper elementum. Vestibulum ut euismod tortor.
+                        Donec dui velit, tempus ac magna at, aliquam laoreet
+                        sapien.
                     </p>
                 </div>
             </div>
@@ -46,5 +53,5 @@ export default function Memories() {
                 {getImages()}
             </LightGallery>
         </div>
-    )
+    );
 }
