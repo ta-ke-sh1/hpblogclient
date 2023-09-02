d236eb78663c9a0fa0a4586c63f6c4fa9fd6fdd1
takesh1 <85833065+ta-ke-sh1@users.noreply.github.com>
2023-09-02 15:46:10 +0700

    contact page

--- src/components/card/storyCard.js
+++ src/components/card/storyCard.js
@@ -1,110 +1,131 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { Grid } from "@mui/material";
-import Draggable from "react-draggable";
 import { BothWays } from "../cursor/commonIcon";
 import MarqueTrack from "../marques/Marque";
 
 export default function StoryCard(props) {
-  const storyCard = useRef(null);
+    const storyCard = useRef(null);
 
-  useEffect(() => {
-    gsap.to(storyCard.current, {
-      left: props.pos.x,
-      top: props.pos.y,
-      duration: 0,
+    useEffect(() => {
+        gsap.to(storyCard.current, {
+            left: props.pos.x,
+            top: props.pos.y,
+            duration: 0,
+        });
     });
-  });
 
-  const cursorNormal = (outer, text) => {
-    gsap.to(outer, {
-      scale: 1,
-      duration: 0.4,
-      ease: "power",
-    });
-    gsap.to(text, {
-      opacity: 0,
-      duration: 0.2,
-      ease: "power",
-    });
-  };
+    const cursorNormal = (outer, text) => {
+        gsap.to(outer, {
+            scale: 1,
+            duration: 0.4,
+            ease: "power",
+        });
+        gsap.to(text, {
+            opacity: 0,
+            duration: 0.2,
+            ease: "power",
+        });
+    };
 
-  const cursorMagnify = (outer, text) => {
-    gsap.to(outer, {
-      scale: 4,
-      duration: 0.4,
-      ease: "power",
-    });
-    gsap.to(text, {
-      opacity: 1,
-      duration: 0.2,
-      ease: "power",
-    });
-  };
+    const cursorMagnify = (outer, text) => {
+        gsap.to(outer, {
+            scale: 4,
+            duration: 0.4,
+            ease: "power",
+        });
+        gsap.to(text, {
+            opacity: 1,
+            duration: 0.2,
+            ease: "power",
+        });
+    };
 
-  const trackOnMouseEnter = () => {
-    let outer = document.getElementById("outer-circle");
-    let text = document.getElementById("cursor-text");
-    cursorMagnify(outer, text);
-  };
+    const trackOnMouseEnter = () => {
+        let outer = document.getElementById("outer-circle");
+        let text = document.getElementById("cursor-text");
+        cursorMagnify(outer, text);
+    };
 
-  const trackOnMouseLeave = () => {
-    let outer = document.getElementById("outer-circle");
-    let text = document.getElementById("cursor-text");
-    cursorNormal(outer, text);
-  };
+    const trackOnMouseLeave = () => {
+        let outer = document.getElementById("outer-circle");
+        let text = document.getElementById("cursor-text");
+        cursorNormal(outer, text);
+    };
 
-  const trackOnMouseDown = () => {
-    console.log("Down");
-  };
+    const trackOnMouseDown = () => {
+        console.log("Down");
+    };
 
-  const trackOnMouseUp = () => {
-    console.log("Up");
-    document.getElementById("cursor-text").appendChild(document.createElement(<BothWays />));
-  };
+    const trackOnMouseUp = () => {
+        console.log("Up");
+        document
+            .getElementById("cursor-text")
+            .appendChild(document.createElement(<BothWays />));
+    };
 
-  const handleClose = () => {
-    gsap.to(storyCard.current, {
-      opacity: 0,
-      pointerEvents: "none",
-      duration: 0,
-    });
-  };
+    const handleClose = () => {
+        gsap.to(storyCard.current, {
+            opacity: 0,
+            pointerEvents: "none",
+            duration: 0,
+        });
+    };
 
-  return (
-    <Draggable onMouseDown={() => trackOnMouseDown()} onMouseUp={() => trackOnMouseUp()}>
-      <div className={"story-card " + props.className} onMouseEnter={() => trackOnMouseEnter()} onMouseLeave={() => trackOnMouseLeave()} ref={storyCard} style={{ width: props.width + "vw" }}>
-        <div className="control">
-          <div className="story-title">{props.title}.txt</div>
-          <button onClick={() => handleClose()}>
-            <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 71.21 71.21">
-              <g id="Layer_2" data-name="Layer 2">
-                <g id="Layer_1-2" data-name="Layer 1">
-                  <polygon className="cls-1" points="71.21 8.9 62.31 0 35.61 26.7 8.9 0 0 8.9 26.7 35.61 0 62.31 8.9 71.21 35.61 44.51 62.31 71.21 71.21 62.31 44.51 35.61 71.21 8.9" />
-                </g>
-              </g>
-            </svg>
-          </button>
-        </div>
-        <div className="content" style={{ maxHeight: props.height + "vh" }}>
-          <MarqueTrack width={props.width} />
-          <div className="content-padding">
-            <>
-              <div style={{ marginBottom: "15px" }}></div>
-              <Grid container>
-                <Grid item xs={4}>
-                  {" "}
-                  <p className="paragraph">
-                    Previously studied Business Japanese at Foreign Trade University of Vietnam, yet somewhere along the line I realized that it was not the right path for me. So I enrolled into University of Greenwich and studied software engineering, mainly because I had been
-                    tinkering with computer since the age of 3.
-                  </p>
-                </Grid>
-                <Grid item xs={6}></Grid>
-              </Grid>
-            </>
-          </div>
-        </div>
-      </div>
-    </Draggable>
-  );
+    return (
+        <>
+            <div
+                className={"story-card " + props.className}
+                onMouseEnter={() => trackOnMouseEnter()}
+                onMouseLeave={() => trackOnMouseLeave()}
+                ref={storyCard}
+                style={{ width: props.width + "vw" }}>
+                <div className="control">
+                    <div className="story-title">{props.title}.txt</div>
+                    <button onClick={() => handleClose()}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width={20}
+                            height={20}
+                            viewBox="0 0 71.21 71.21">
+                            <g id="Layer_2" data-name="Layer 2">
+                                <g id="Layer_1-2" data-name="Layer 1">
+                                    <polygon
+                                        className="cls-1"
+                                        points="71.21 8.9 62.31 0 35.61 26.7 8.9 0 0 8.9 26.7 35.61 0 62.31 8.9 71.21 35.61 44.51 62.31 71.21 71.21 62.31 44.51 35.61 71.21 8.9"
+                                    />
+                                </g>
+                            </g>
+                        </svg>
+                    </button>
+                </div>
+                <div
+                    className="content"
+                    style={{ maxHeight: props.height + "vh" }}>
+                    <MarqueTrack width={props.width} />
+                    <div className="content-padding">
+                        <>
+                            <div style={{ marginBottom: "15px" }}></div>
+                            <Grid container>
+                                <Grid item xs={4}>
+                                    {" "}
+                                    <p className="paragraph">
+                                        Previously studied Business Japanese at
+                                        Foreign Trade University of Vietnam, yet
+                                        somewhere along the line I realized that
+                                        it was not the right path for me. So I
+                                        enrolled into University of Greenwich
+                                        and studied software engineering, mainly
+                                        because I had been tinkering with
+                                        computer since the age of 3.
+                                    </p>
+                                </Grid>
+                                <Grid item xs={6}></Grid>
+                            </Grid>
+                        </>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
--- src/views/blog/details.js
+++ src/views/blog/details.js
@@ -4,262 +4,295 @@ import { textShuffle } from "../../utils/utils";
 import { gsap } from "gsap";
 
 export default function BlogDetails({ props }) {
-  const posterTrackContainer = useRef(null);
-  const blogTitle = useRef(null);
-  const [lastIndex, setLast] = useState(-1);
-  const [posters, setPosters] = useState([]);
+    const posterTrackContainer = useRef(null);
+    const blogTitle = useRef(null);
+    const [lastIndex, setLast] = useState(-1);
+    const [posters, setPosters] = useState([]);
 
-  const cursorNormal = (outer, text) => {
-    gsap.to(outer, {
-      scale: 1,
-      duration: 0.4,
-      ease: "power",
+    const cursorNormal = (outer, text) => {
+        gsap.to(outer, {
+            scale: 1,
+            duration: 0.4,
+            ease: "power",
+        });
+        gsap.to(text, {
+            opacity: 0,
+            duration: 0.2,
+            ease: "power",
+        });
+    };
+
+    const cursorMagnify = (outer, text) => {
+        gsap.to(outer, {
+            scale: 4,
+            duration: 0.4,
+            ease: "power",
+        });
+        gsap.to(text, {
+            opacity: 1,
+            duration: 0.2,
+            ease: "power",
+        });
+    };
+
+    useEffect(() => {
+        let icon = document.getElementById("svg-cursor-icon");
+        if (icon) {
+            gsap.to(icon, {
+                transform: "rotate(0deg)",
+                duration: 0.4,
+                ease: "power",
+            });
+        }
     });
-    gsap.to(text, {
-      opacity: 0,
-      duration: 0.2,
-      ease: "power",
-    });
-  };
 
-  const cursorMagnify = (outer, text) => {
-    gsap.to(outer, {
-      scale: 4,
-      duration: 0.4,
-      ease: "power",
-    });
-    gsap.to(text, {
-      opacity: 1,
-      duration: 0.2,
-      ease: "power",
-    });
-  };
+    let interval = null;
 
-  useEffect(() => {
+    const titleOnMouseOver = (index) => {
+        let title = document.getElementById("poster-title-" + index);
+        let textContent = title.getAttribute("data-original-text");
+        textShuffle(
+            title,
+            textContent,
+            interval,
+            300 / posterContent[index].name.length
+        );
 
-    let icon = document.getElementById('svg-cursor-icon')
-    if (icon) {
-      gsap.to(icon, {
-        transform: 'rotate(0deg)',
-        duration: 0.4,
-        ease: "power",
-      })
-    }
-  });
+        let newDiv = imageDiv(index);
+        if (index !== lastIndex && posters.length < 5) {
+            setLast(index);
+            setPosters([...posters, newDiv]);
+        } else {
+            setLast(index);
+            setPosters([...posters, newDiv].slice(1));
+        }
+    };
 
-  let interval = null;
+    const trackOnMouseEnter = () => {
+        let outer = document.getElementById("outer-circle");
+        let text = document.getElementById("cursor-text");
+        cursorMagnify(outer, text);
+    };
 
-  const titleOnMouseOver = (index) => {
+    const trackOnMouseLeave = () => {
+        let outer = document.getElementById("outer-circle");
+        let text = document.getElementById("cursor-text");
+        cursorNormal(outer, text);
+    };
 
-    let title = document.getElementById("poster-title-" + index);
-    let textContent = title.getAttribute("data-original-text");
-    textShuffle(title, textContent, interval, 300 / posterContent[index].name.length);
-
-    let newDiv = imageDiv(index);
-    if (index !== lastIndex && posters.length < 5) {
-      setLast(index);
-      setPosters([...posters, newDiv]);
-    } else {
-      setLast(index);
-      setPosters([...posters, newDiv].slice(1));
-    }
-  };
-
-  const trackOnMouseEnter = () => {
-    let outer = document.getElementById('outer-circle')
-    let text = document.getElementById('cursor-text');
-    cursorMagnify(outer, text);
-  }
-
-  const trackOnMouseLeave = () => {
-    let outer = document.getElementById('outer-circle')
-    let text = document.getElementById('cursor-text');
-    cursorNormal(outer, text);
-  }
-
-  return (
-    <m.div
-      initial={{ y: "100%" }}
-      animate={{ y: "0%" }}
-      exit={{ y: "-100%" }}
-      className="custom-wrapper"
-      transition={{
-        duration: 0.75,
-        ease: "easeOut",
-      }}
-    >
-      <div className="blog-details-wrapper">
-        <div className="blog-text med">
-          <p id="blog-title" ref={blogTitle}>
-            30 days
-          </p>
-        </div>
-        <div className="blog-details-content">
-          <div className="track" ref={posterTrackContainer} id="posters-track" onMouseEnter={() => trackOnMouseEnter()} onMouseLeave={() => trackOnMouseLeave()}>
-            {numbers.map((i, index) => {
-              return (
-                <div className="poster-title" key={"poster-" + index}>
-                  <h1 onMouseEnter={() => titleOnMouseOver(index)} className="blog-title noselect" id={"poster-title-" + index} data-original-text={posterContent[index].name}>
-                    {posterContent[index].name.toLowerCase()}
-                  </h1>
-                  <span className="title-number med noselect">({index + 1})</span>
+    return (
+        <m.div
+            initial={{ y: "100%" }}
+            animate={{ y: "0%" }}
+            exit={{ y: "-100%" }}
+            className="custom-wrapper"
+            transition={{
+                duration: 0.75,
+                ease: "easeOut",
+            }}>
+            <div className="blog-details-wrapper">
+                <div className="blog-text med">
+                    <p id="blog-title" ref={blogTitle}>
+                        30 days
+                    </p>
                 </div>
-              );
-            })}
-          </div>
-        </div>
-        <div className="blog-image-container">
-          <div className="poster-holder">{posters.map((poster, index) => <div key={"poster-" + index} className="blog-img">{poster}</div>)}</div>
-        </div>
-        <div className="navigation-guide med">
-          <p>Scroll</p>
-        </div>
-      </div>
-    </m.div>
-  );
+                <div className="blog-details-content">
+                    <div
+                        className="track"
+                        ref={posterTrackContainer}
+                        id="posters-track"
+                        onMouseEnter={() => trackOnMouseEnter()}
+                        onMouseLeave={() => trackOnMouseLeave()}>
+                        {numbers.map((i, index) => {
+                            return (
+                                <div
+                                    className="poster-title"
+                                    key={"poster-" + index}>
+                                    <h1
+                                        onMouseEnter={() =>
+                                            titleOnMouseOver(index)
+                                        }
+                                        className="blog-title noselect"
+                                        id={"poster-title-" + index}
+                                        data-original-text={
+                                            posterContent[index].name
+                                        }>
+                                        {posterContent[
+                                            index
+                                        ].name.toLowerCase()}
+                                    </h1>
+                                    <span className="title-number med noselect">
+                                        ({index + 1})
+                                    </span>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+                <div className="blog-image-container">
+                    <div className="poster-holder">
+                        {posters.map((poster, index) => (
+                            <div key={"poster-" + index} className="blog-img">
+                                {poster}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="navigation-guide med">
+                    <p>Scroll</p>
+                </div>
+            </div>
+        </m.div>
+    );
 }
 
 const imageDiv = (i) => {
-  return (
-
-    <img
-      alt="flower"
-      className="img-div"
-      src={process.env.PUBLIC_URL + "/projects/30_days/day " + (i + 1) + ".jpg"}
-      style={{
-        transform: "rotate(" + randomNumberInRange(-20, 20) + "deg)",
-      }}
-    />
-
-  );
+    return (
+        <img
+            alt="flower"
+            className="img-div"
+            src={
+                process.env.PUBLIC_URL +
+                "/projects/30_days/day " +
+                (i + 1) +
+                ".jpg"
+            }
+            style={{
+                transform: "rotate(" + randomNumberInRange(-20, 20) + "deg)",
+            }}
+        />
+    );
 };
 
 function randomNumberInRange(min, max) {
-  // 👇️ get number between min (inclusive) and max (inclusive)
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    // 👇️ get number between min (inclusive) and max (inclusive)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+const numbers = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30,
+];
 
 const posterContent = [
-  {
-    name: "Convergence",
-    info: "",
-  },
-  {
-    name: "Mars",
-    info: "",
-  },
-  {
-    name: "Manifest",
-    info: "",
-  },
-  {
-    name: "Sunburst",
-    info: "",
-  },
-  {
-    name: "Summertime",
-    info: "",
-  },
-  {
-    name: "Pressure",
-    info: "",
-  },
-  {
-    name: "Liberation",
-    info: "",
-  },
-  {
-    name: "Radical",
-    info: "",
-  },
-  {
-    name: "Solitude",
-    info: "",
-  },
-  {
-    name: "Accelerate",
-    info: "",
-  },
-  {
-    name: "Sunflower",
-    info: "",
-  },
-  {
-    name: "Conflict of Interest",
-    info: "",
-  },
-  {
-    name: "Polaroid",
-    info: "",
-  },
-  {
-    name: "Morning Coffee",
-    info: "",
-  },
-  {
-    name: "Labyrinth",
-    info: "",
-  },
-  {
-    name: "Existential",
-    info: "",
-  },
-  {
-    name: "Run!!!",
-    info: "",
-  },
-  {
-    name: "Disoriented",
-    info: "",
-  },
-  {
-    name: "Intertwined",
-    info: "",
-  },
-  {
-    name: "Psyched",
-    info: "",
-  },
-  {
-    name: "Daily Process",
-    info: "",
-  },
-  {
-    name: "Superfluid",
-    info: "",
-  },
-  {
-    name: "Void",
-    info: "",
-  },
-  {
-    name: "Diagnosis",
-    info: "",
-  },
-  {
-    name: "Where do we belong?",
-    info: "",
-  },
-  {
-    name: "Gradient",
-    info: "",
-  },
-  {
-    name: "Spacewalker",
-    info: "",
-  },
-  {
-    name: "Lost in Translation",
-    info: "",
-  },
-  {
-    name: "Break your limit",
-    info: "",
-  },
-  {
-    name: "El Paradiso",
-    info: "",
-  },
+    {
+        name: "Convergence",
+        info: "",
+    },
+    {
+        name: "Mars",
+        info: "",
+    },
+    {
+        name: "Manifest",
+        info: "",
+    },
+    {
+        name: "Sunburst",
+        info: "",
+    },
+    {
+        name: "Summertime",
+        info: "",
+    },
+    {
+        name: "Pressure",
+        info: "",
+    },
+    {
+        name: "Liberation",
+        info: "",
+    },
+    {
+        name: "Radical",
+        info: "",
+    },
+    {
+        name: "Solitude",
+        info: "",
+    },
+    {
+        name: "Accelerate",
+        info: "",
+    },
+    {
+        name: "Sunflower",
+        info: "",
+    },
+    {
+        name: "Conflict of Interest",
+        info: "",
+    },
+    {
+        name: "Polaroid",
+        info: "",
+    },
+    {
+        name: "Morning Coffee",
+        info: "",
+    },
+    {
+        name: "Labyrinth",
+        info: "",
+    },
+    {
+        name: "Existential",
+        info: "",
+    },
+    {
+        name: "Run!!!",
+        info: "",
+    },
+    {
+        name: "Disoriented",
+        info: "",
+    },
+    {
+        name: "Intertwined",
+        info: "",
+    },
+    {
+        name: "Psyched",
+        info: "",
+    },
+    {
+        name: "Daily Process",
+        info: "",
+    },
+    {
+        name: "Superfluid",
+        info: "",
+    },
+    {
+        name: "Void",
+        info: "",
+    },
+    {
+        name: "Diagnosis",
+        info: "",
+    },
+    {
+        name: "Where do we belong?",
+        info: "",
+    },
+    {
+        name: "Gradient",
+        info: "",
+    },
+    {
+        name: "Spacewalker",
+        info: "",
+    },
+    {
+        name: "Lost in Translation",
+        info: "",
+    },
+    {
+        name: "Break your limit",
+        info: "",
+    },
+    {
+        name: "El Paradiso",
+        info: "",
+    },
 ];
