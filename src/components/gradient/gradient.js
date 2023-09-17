3e4e041920e2a4651dbaeb827dd82e87b088ff7a
takesh1 <85833065+ta-ke-sh1@users.noreply.github.com>
2023-09-17 23:00:06 +0700

    update to video display

--- src/components/gradient/gradient.js
+++ src/components/gradient/gradient.js
@@ -1,14 +1,21 @@
-import React from "react"
+import React from "react";
 
 export default function GradientMap(props) {
-
     return (
-        <div className="entrance-image" style={{
-
-        }}>
-            <div className="isolate">
-                <div className="overlay"></div>
-            </div>
+        <div className="entrance-image">
+            <video
+                className="overlay"
+                src={`${process.env.PUBLIC_URL + "/about/vid.mp4"}`}
+                autoPlay
+                loop></video>
         </div>
-    )
+    );
 }
+
+const div = (
+    <div className="entrance-image">
+        <div className="isolate">
+            <div className="overlay"></div>
+        </div>
+    </div>
+);
